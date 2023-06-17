83887f7549567ba6e075a41f1ddf0abd280dfded
Genesis Scott <chandlerscott123@gmail.com>
2023-06-17 11:21:05 -0500

    add load event listener to window

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,11 @@
 
 
+function suggestProgrammingLanguage() {
+}
+
+
 
 window.addEventListener('load', function() {
   let form = document.getElementById('survey');
+  form.addEventListener('submit', suggestProgrammingLanguage);
 });
